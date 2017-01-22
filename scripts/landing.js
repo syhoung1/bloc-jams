0093b8b8023ba3811edc127e8e364604fb82f50c
syhoung1 <syhoung1@gmail.com>
2017-01-22 00:00:55 -0800

    assignment 8

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,15 @@
 var animatePoints = function() {
  
                  var points = document.getElementsByClassName('point');
- 
+                 
+    
+                 var revealPoint = function(a) {
+                     points[a].style.opacity = 1;
+                     points[a].style.transform = "scaleX(1) translateY(0)";
+                     points[a].style.msTransform = "scaleX(1) translateY(0)";
+                     points[a].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 };
+    
                  var revealFirstPoint = function() {
                      points[0].style.opacity = 1;
                      points[0].style.transform = "scaleX(1) translateY(0)";
@@ -23,9 +31,9 @@ var animatePoints = function() {
                      points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
                  };
  
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
+                 for(var i = 0; i<points.length;i++){
+                     revealPoint(i);
+                 }
  
              };
         
