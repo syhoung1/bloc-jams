cf651d55c788bc8b5090214771edec62750095d8
syhoung1 <syhoung1@gmail.com>
2017-01-22 21:43:48 -0800

    checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,15 @@
-var animatePoints = function() {
- 
-                 var points = document.getElementsByClassName('point');
- 
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+                 
+    
+                 var revealPoint = function(a) {
+                     points[a].style.opacity = 1;
+                     points[a].style.transform = "scaleX(1) translateY(0)";
+                     points[a].style.msTransform = "scaleX(1) translateY(0)";
+                     points[a].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 };
+    
                  var revealFirstPoint = function() {
                      points[0].style.opacity = 1;
                      points[0].style.transform = "scaleX(1) translateY(0)";
@@ -23,10 +31,29 @@ var animatePoints = function() {
                      points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
                  };
  
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
+                 for(var i = 0; i<points.length;i++){
+                     revealPoint(i);
+                 }
  
              };
-        
+
+window.onload = function() {
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+    
+     window.addEventListener("scroll", function(event) {
+         
+         
+         if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         
+         }
+           
+ });
+}
         
