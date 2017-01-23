73a657b19b5f5c9bdba8ca932d6eab271be710c3
syhoung1 <syhoung1@gmail.com>
2017-01-22 22:59:28 -0800

    assignment 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,42 +1,13 @@
-
-var animatePoints = function() {
- 
-                 var points = document.getElementsByClassName('point');
-
-    
-                 var revealPoint = function(a) {
-                     points[a].style.opacity = 1;
-                     points[a].style.transform = "scaleX(1) translateY(0)";
-                     points[a].style.msTransform = "scaleX(1) translateY(0)";
-                     points[a].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
-    
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 for(var i = 0; i<points.length;i++){
-                     revealPoint(i);
-                 }
- 
-             };
+var pointsArray = document.getElementsByClassName('point');
+var revealPoint = function(point) {
+                     point.style.opacity = 1;
+                     point.style.transform = "scaleX(1) translateY(0)";
+                     point.style.msTransform = "scaleX(1) translateY(0)";
+                     point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+var animatePoints = function(points) {
+     forEach(points,revealPoint);
+};
 
 window.onload = function() {
      var sellingPoints = document.getElementsByClassName('selling-points')[0];
---
+++ scripts/utilities.js
@@ -0,0 +1,9 @@
+function forEach(array, callback) {
+    for(var i = 0; i < array.length;i++) {
+        callback(array[i]);
+    }
+    return 0;
+};
+
+
+
