2d99a461a9d5dc9f8c2305a7922163436ebc5323
syhoung1 <syhoung1@gmail.com>
2017-01-24 11:48:30 -0800

    checkpoint 11

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,21 @@
-
 var animatePoints = function() {
  
-                 var points = document.getElementsByClassName('point');
+     var points = document.getElementsByClassName('point');
 
     
-                 var revealPoint = function (a) {
-                     points[a].style.opacity = 1;
-                     points[a].style.transform = "scaleX(1) translateY(0)";
-                     points[a].style.msTransform = "scaleX(1) translateY(0)";
-                     points[a].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
+     var revealPoint = function (a) {
+         points[a].style.opacity = 1;
+         points[a].style.transform = "scaleX(1) translateY(0)";
+         points[a].style.msTransform = "scaleX(1) translateY(0)";
+         points[a].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
 
- 
-                 for(var i = 0; i<points.length;i++){
-                     revealPoint(i);
-                 }
- 
-             };
+
+     for(var i = 0; i<points.length;i++){
+         revealPoint(i);
+     }
+
+ };
 
 window.onload = function() {
      var sellingPoints = document.getElementsByClassName('selling-points')[0];
